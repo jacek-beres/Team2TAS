460438dc7d567db3d422ada837ca02990f166d66
inga-g <inga.gradzik@gmail.com>
2023-01-22 20:11:53 +0100

    Naprawa wyswietlanych komunikatow

--- program.py
+++ program.py
@@ -11,13 +11,13 @@ if wiek.isdigit() == False:
 #Zamiana wieku na typ int
 wiek = int(wiek)
 
-if wiek >= 18 and wiek<=40:
-    print("Witaj w naszej apce z alkoholem, zapraszamy na zakupy.")
-elif wiek > 40:
-    print("Witaj w naszej apce z alkoholem, zapraszamy na zakupy.")
-    print("Uważaj w Twoim wieku na alkohol.")
-else:
-    print("Jesteś za młody/a na alkohol.")
+# if wiek >= 18 and wiek<=40:
+#     print("Witaj w naszej apce z alkoholem, zapraszamy na zakupy.")
+# elif wiek > 40:
+#     print("Witaj w naszej apce z alkoholem, zapraszamy na zakupy.")
+#     print("Uważaj w Twoim wieku na alkohol.")
+# else:
+#     print("Jesteś za młody/a na alkohol.")
 
 region = str(region)
 plec = str(plec)
@@ -31,8 +31,7 @@ if region == "USA":
     if wiek >= 21 and wiek < 30:
         print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
     elif wiek >= 30 and (plec == "K" or plec == "k"):
-        print("Witaj w naszej apce z alkoholem. Zapraszamy do zakupów. 
-        Mamy dla Ciebie wyjatkowy prezent. Aperol Spritz doda sie automatycznie do Twojego koszyka z zakupami")
+        print("Witaj w naszej apce z alkoholem. Zapraszamy do zakupów.\nMamy dla Ciebie wyjatkowy prezent. Aperol Spritz doda sie automatycznie do Twojego koszyka z zakupami")
         print("Darmowy aperol dla Ciebie kochana")
     elif wiek >= 30 and (plec == "M" or plec == "m"):
         print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
@@ -43,9 +42,10 @@ if region == "EUR":
     print("Twój region to EUR")
     if wiek >= 18 and wiek < 30:
         print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-    elif wiek >= 30 and plec == "K":
-        print("Witaj w naszej apce z alkoholem. Zapraszamy do zakupów. Mamy dla Ciebie wyjatkowy prezent. Aperol Spritz doda sie automatycznie do Twojego koszyka z zakupami")
-    elif wiek >= 30 and plec == "M":
+    elif wiek >= 30 and (plec == "K" or plec == "k"):
+        print("Witaj w naszej apce z alkoholem. Zapraszamy do zakupów.\nMamy dla Ciebie wyjatkowy prezent. Aperol Spritz doda sie automatycznie do Twojego koszyka z zakupami")
+        print("Darmowy aperol dla Ciebie kochana")
+    elif wiek >= 30 and (plec == "M" or plec == "m"):
         print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
     else:
         exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
